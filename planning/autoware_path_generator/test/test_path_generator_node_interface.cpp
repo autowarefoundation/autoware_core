6d7b23f06fbece680176fae5376af733968ae641
t4-adc <grp-rd-1-adc-admin@tier4.jp>
2025-04-15 16:35:55 +0900

    fix test include path
    Signed-off-by: t4-adc <grp-rd-1-adc-admin@tier4.jp>

--- planning/autoware_path_generator/test/test_path_generator_node_interface.cpp
+++ planning/autoware_path_generator/test/test_path_generator_node_interface.cpp
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "autoware/path_generator/node.hpp"
+
 #include <ament_index_cpp/get_package_share_directory.hpp>
-#include <autoware/path_generator/node.hpp>
 #include <autoware/planning_test_manager/autoware_planning_test_manager.hpp>
 #include <autoware_test_utils/autoware_test_utils.hpp>
 
