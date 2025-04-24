6fc453816ee5733d3952cec803fcc58a807554df
mitukou1109 <mitukou1109@gmail.com>
2025-04-24 16:52:09 +0900

    deprecate extendLine() instead of removing it
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/include/autoware/behavior_velocity_planner_common/utilization/util.hpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/include/autoware/behavior_velocity_planner_common/utilization/util.hpp
@@ -126,6 +126,10 @@ double findReachTime(
 
 std::vector<geometry_msgs::msg::Point> toRosPoints(const PredictedObjects & object);
 
+[[deprecated]] LineString2d extendLine(
+  const lanelet::ConstPoint3d & lanelet_point1, const lanelet::ConstPoint3d & lanelet_point2,
+  const double & length);
+
 /**
  * @brief Extend segment until it intersects with two bounds
  * @param segment Segment to extend
