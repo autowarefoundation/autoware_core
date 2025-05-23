f7199d4d5657accf805274340834346974d1ef1e
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2025-05-23 11:29:23 +0900

    refactor: remove filter base class from random_downsample_filter
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- sensing/autoware_downsample_filters/src/random_downsample_filter/random_downsample_filter_node.cpp
+++ sensing/autoware_downsample_filters/src/random_downsample_filter/random_downsample_filter_node.cpp
@@ -11,54 +11,184 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2009, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- */
 
-#include "autoware/downsample_filters/random_downsample_filter_node.hpp"
 
+#include "random_downsample_filter_node.hpp"
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.hpp>
+#include <tf2_eigen/tf2_eigen.hpp>
+
+#include <string>
 #include <vector>
 
 namespace autoware::downsample_filters
 {
 RandomDownsampleFilter::RandomDownsampleFilter(const rclcpp::NodeOptions & options)
-: Filter("RandomDownsampleFilter", options)
+: Node("RandomDownsampleFilter", options)
 {
   // set initial parameters
   {
+    tf_input_frame_ = declare_parameter<std::string>("input_frame");
+    tf_output_frame_ = declare_parameter<std::string>("output_frame");
+    max_queue_size_ = static_cast<std::size_t>(declare_parameter<int64_t>("max_queue_size"));
     sample_num_ = static_cast<size_t>(declare_parameter<int64_t>("sample_num"));
+
+    RCLCPP_DEBUG_STREAM(
+      this->get_logger(),
+      "Filter (as Component) successfully created with the following parameters:"
+        << std::endl
+        << " - max_queue_size   : " << max_queue_size_ << std::endl
+        << " - sample_num       : " << sample_num_);
   }
+
+  // Set publisher
+  {
+    rclcpp::PublisherOptions pub_options;
+    pub_options.qos_overriding_options = rclcpp::QosOverridingOptions::with_default_policies();
+    pub_output_ = this->create_publisher<PointCloud2>(
+      "output", rclcpp::SensorDataQoS().keep_last(max_queue_size_), pub_options);
+
+    published_time_publisher_ = std::make_unique<autoware_utils_debug::PublishedTimePublisher>(this);
+  }
+
+  // Set subscriber
+  {
+    sub_input_ = create_subscription<PointCloud2>(
+      "input", rclcpp::SensorDataQoS().keep_last(max_queue_size_), 
+      std::bind(&RandomDownsampleFilter::input_callback, this, std::placeholders::_1));
+    transform_listener_ = std::make_unique<autoware_utils_tf::TransformListener>(this);
+  }
+
+  RCLCPP_DEBUG(this->get_logger(), "[Filter Constructor] successfully created.");
+}
+
+void RandomDownsampleFilter::input_callback(const PointCloud2ConstPtr cloud){
+  // If cloud is given, check if it's valid
+  if (!is_valid(cloud)) {
+    RCLCPP_ERROR(this->get_logger(), "[input_callback] Invalid input!");
+    return;
+  }
+
+  RCLCPP_DEBUG(
+    this->get_logger(),
+    "[input_callback] PointCloud with %d data points and frame %s on input topic "
+    "received.",
+    cloud->width * cloud->height, cloud->header.frame_id.c_str());
+
+  // Check whether the user has given a different input TF frame
+  tf_input_orig_frame_ = cloud->header.frame_id;
+  PointCloud2ConstPtr cloud_tf;
+  if (!tf_input_frame_.empty() && cloud->header.frame_id != tf_input_frame_) {
+    RCLCPP_DEBUG(
+      this->get_logger(), "[input_callback] Transforming input dataset from %s to %s.",
+      cloud->header.frame_id.c_str(), tf_input_frame_.c_str());
+
+    // Save the original frame ID
+    // Convert the cloud into the different frame
+    auto cloud_transformed = std::make_unique<PointCloud2>();
+
+    auto tf_ptr = transform_listener_->get_transform(
+      tf_output_frame_, cloud_tf->header.frame_id, cloud_tf->header.stamp,
+      rclcpp::Duration::from_seconds(1.0));
+    if (!tf_ptr) {
+      RCLCPP_ERROR(
+        this->get_logger(),
+        "[input_callback] Error converting output dataset from %s to %s.",
+        cloud_tf->header.frame_id.c_str(), tf_output_frame_.c_str());
+      return;
+    }
+
+    auto eigen_tf = tf2::transformToEigen(*tf_ptr);
+    pcl_ros::transformPointCloud(eigen_tf.matrix().cast<float>(), *cloud, *cloud_transformed);
+    cloud_tf = std::move(cloud_transformed);
+
+  } else {
+    cloud_tf = cloud;
+  }
+
+  compute_publish(cloud_tf);
+
+}
+
+bool RandomDownsampleFilter::is_valid(const PointCloud2ConstPtr & cloud)
+{
+  if (cloud->width * cloud->height * cloud->point_step != cloud->data.size()) {
+    RCLCPP_WARN(
+      this->get_logger(),
+      "Invalid PointCloud (data = %zu, width = %d, height = %d, step = %d) with stamp %f, "
+      "and frame %s received!",
+      cloud->data.size(), cloud->width, cloud->height, cloud->point_step,
+      rclcpp::Time(cloud->header.stamp).seconds(), cloud->header.frame_id.c_str());
+    return false;
+  }
+  return true;
+}
+
+void RandomDownsampleFilter::compute_publish(const PointCloud2ConstPtr & input)
+{
+  auto output = std::make_unique<PointCloud2>();
+  filter(input, *output);
+  if (!convert_output_costly(output)) return;
+
+  // Copy timestamp to keep it
+  output->header.stamp = input->header.stamp;
+
+  // Publish a boost shared ptr
+  pub_output_->publish(std::move(output));
+  published_time_publisher_->publish_if_subscribed(pub_output_, input->header.stamp);
+}
+
+bool RandomDownsampleFilter::convert_output_costly(std::unique_ptr<PointCloud2> & output){
+    // In terms of performance, we should avoid using pcl_ros library function,
+  // but this code path isn't reached in the main use case of Autoware, so it's left as is for now.
+  if (!tf_output_frame_.empty() && output->header.frame_id != tf_output_frame_) {
+    RCLCPP_DEBUG(
+      this->get_logger(), "[convert_output_costly] Transforming output dataset from %s to %s.",
+      output->header.frame_id.c_str(), tf_output_frame_.c_str());
+
+    // Convert the cloud into the different frame
+    auto cloud_transformed = std::make_unique<PointCloud2>();
+
+    auto tf_ptr = transform_listener_->get_transform(
+      tf_output_frame_, output->header.frame_id, output->header.stamp,
+      rclcpp::Duration::from_seconds(1.0));
+    if (!tf_ptr) {
+      RCLCPP_ERROR(
+        this->get_logger(),
+        "[convert_output_costly] Error converting output dataset from %s to %s.",
+        output->header.frame_id.c_str(), tf_output_frame_.c_str());
+      return false;
+    }
+
+    auto eigen_tf = tf2::transformToEigen(*tf_ptr);
+    pcl_ros::transformPointCloud(eigen_tf.matrix().cast<float>(), *output, *cloud_transformed);
+    output = std::move(cloud_transformed);
+  }
+
+  // Same as the comment above
+  if (tf_output_frame_.empty() && output->header.frame_id != tf_input_orig_frame_) {
+    // No tf_output_frame given, transform the dataset to its original frame
+    RCLCPP_DEBUG(
+      this->get_logger(), "[convert_output_costly] Transforming output dataset from %s back to %s.",
+      output->header.frame_id.c_str(), tf_input_orig_frame_.c_str());
+
+    auto cloud_transformed = std::make_unique<sensor_msgs::msg::PointCloud2>();
+
+    auto tf_ptr = transform_listener_->get_transform(
+      tf_input_orig_frame_, output->header.frame_id, output->header.stamp,
+      rclcpp::Duration::from_seconds(1.0));
+
+    if (!tf_ptr) {
+      return false;
+    }
+
+    auto eigen_tf = tf2::transformToEigen(*tf_ptr);
+    pcl_ros::transformPointCloud(eigen_tf.matrix().cast<float>(), *output, *cloud_transformed);
+    output = std::move(cloud_transformed);
+  }
+
+  return true;
 }
 
 void RandomDownsampleFilter::filter(const PointCloud2ConstPtr & input, PointCloud2 & output)
--- sensing/autoware_downsample_filters/src/random_downsample_filter/random_downsample_filter_node.hpp
+++ sensing/autoware_downsample_filters/src/random_downsample_filter/random_downsample_filter_node.hpp
@@ -11,65 +11,80 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2009, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * $Id: voxel_grid.cpp 35876 2011-02-09 01:04:36Z rusu $
- *
- */
 
 #ifndef AUTOWARE__DOWNSAMPLE_FILTERS__RANDOM_DOWNSAMPLE_FILTER_NODE_HPP_
 #define AUTOWARE__DOWNSAMPLE_FILTERS__RANDOM_DOWNSAMPLE_FILTER_NODE_HPP_
 
-#include "autoware/downsample_filters/filter.hpp"
 
+#include <autoware_utils_debug/debug_publisher.hpp>
+#include <autoware_utils_debug/published_time_publisher.hpp>
+#include <autoware_utils_system/stop_watch.hpp>
+#include <autoware_utils_tf/transform_listener.hpp>
 #include <pcl/filters/random_sample.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+#include <boost/thread/mutex.hpp>
 
 #include <vector>
+#include <string>
+
 
 namespace autoware::downsample_filters
 {
-class RandomDownsampleFilter : public autoware::downsample_filters::Filter
+class RandomDownsampleFilter : public rclcpp::Node
 {
-protected:
-  void filter(const PointCloud2ConstPtr & input, PointCloud2 & output) override;
-
-private:
-  size_t sample_num_;
-
 public:
+  using PointCloud2 = sensor_msgs::msg::PointCloud2;
+  using PointCloud2ConstPtr = sensor_msgs::msg::PointCloud2::ConstSharedPtr;
+
   PCL_MAKE_ALIGNED_OPERATOR_NEW
   explicit RandomDownsampleFilter(const rclcpp::NodeOptions & options);
+
+
+private:
+  void filter(const PointCloud2ConstPtr & input, PointCloud2 & output);
+
+  /** \brief The input TF frame the data should be transformed into,
+   * if input.header.frame_id is different. */
+  std::string tf_input_frame_;
+
+  /** \brief The original data input TF frame. */
+  std::string tf_input_orig_frame_;
+
+  /** \brief The output TF frame the data should be transformed into,
+   * if input.header.frame_id is different. */
+  std::string tf_output_frame_;
+
+  /** \brief Internal mutex. */
+  std::mutex mutex_;
+
+  /** \brief The input PointCloud2 subscriber. */
+  rclcpp::Subscription<PointCloud2>::SharedPtr sub_input_;
+
+  /** \brief The output PointCloud2 publisher. */
+  rclcpp::Publisher<PointCloud2>::SharedPtr pub_output_;
+
+    /** \brief processing time publisher. **/
+  std::unique_ptr<autoware_utils_system::StopWatch<std::chrono::milliseconds>> stop_watch_ptr_;
+  std::unique_ptr<autoware_utils_debug::DebugPublisher> debug_publisher_;
+  std::unique_ptr<autoware_utils_debug::PublishedTimePublisher> published_time_publisher_;
+
+  /** \brief The transform listener. */
+  std::unique_ptr<autoware_utils_tf::TransformListener> transform_listener_{nullptr};
+
+  void input_callback(const PointCloud2ConstPtr cloud);
+
+  bool is_valid(const PointCloud2ConstPtr & cloud);
+
+  void compute_publish(const PointCloud2ConstPtr & input);
+
+  bool convert_output_costly(std::unique_ptr<PointCloud2> & output);
+
+  size_t sample_num_;
+
+  /** \brief The maximum queue size (default: 3). */
+  size_t max_queue_size_ = 3;
 };
 }  // namespace autoware::downsample_filters
 
