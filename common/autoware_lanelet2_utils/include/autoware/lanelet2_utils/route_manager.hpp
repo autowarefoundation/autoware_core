b3b1de15a6226f85bd4b77ff8b0c0aa44421bdf4
Mamoru Sobue <mamoru.sobue@tier4.jp>
2025-05-30 19:57:03 +0900

    adding route_manager
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- common/autoware_lanelet2_utils/include/autoware/lanelet2_utils/route_manager.hpp
+++ common/autoware_lanelet2_utils/include/autoware/lanelet2_utils/route_manager.hpp
@@ -33,17 +33,49 @@ namespace autoware::experimental::lanelet2_utils
 class RouteManager
 {
 public:
+  /**
+   * @brief create RouteManager
+   * @param map_msg raw message
+   * @param route_msg raw message
+   * @param initial_pose initial_pose
+   * @pre route_msg has non-empty RouteSegment
+   * @post current_lanelet is determined, but it is not assured that initial_pose is within
+   * current_lanelet
+   */
   static std::optional<RouteManager> create(
     const autoware_map_msgs::msg::LaneletMapBin & map_msg,
-    const autoware_planning_msgs::msg::LaneletRoute & route_msg);
+    const autoware_planning_msgs::msg::LaneletRoute & route_msg,
+    const geometry_msgs::msg::Pose & initial_pose);
 
+  /**
+   * @brief update current_pose only within forward/backward route lanelets, and return a new
+   * RouteManager if current_pose is valid
+   * @note this function updates current_route_lanelet along its LaneSequence. even if ego is
+   * executing avoidance and deviates from reference path, this function tracks
+   * current_route_lanelet without considering lane change
+   * @attention this can only be called against std::move(*this)
+   */
   std::optional<RouteManager> update_current_pose(const geometry_msgs::msg::Pose & current_pose) &&;
+
+  /**
+   * @brief update current_pose against all route lanelets and return a new RouteManager if
+   * current_pose is valid
+   * @note this functions is aimed to be used only when lane change is completed
+   * @attention this can only be called against std::move(*this)
+   */
+  std::optional<RouteManager> update_current_pose_with_lane_change(
+    const geometry_msgs::msg::Pose & current_pose) &&;
+
+  const lanelet::ConstLanelet & current_lanelet() const { return current_lanelet_; }
 
 private:
   RouteManager(
     lanelet::LaneletMapConstPtr lanelet_map_ptr,
     lanelet::routing::RoutingGraphConstPtr routing_graph_ptr,
-    lanelet::traffic_rules::TrafficRulesPtr traffic_rules_ptr);
+    lanelet::traffic_rules::TrafficRulesPtr traffic_rules_ptr,
+    lanelet::ConstLanelets && all_route_lanelets, lanelet::ConstLanelets && preferred_lanelets,
+    const lanelet::ConstLanelet & start_lanelet, const lanelet::ConstLanelet & goal_lanelet,
+    const geometry_msgs::msg::Pose & current_pose, lanelet::ConstLanelet current_lanelet);
 
   lanelet::LaneletMapConstPtr lanelet_map_ptr_;
   lanelet::routing::RoutingGraphConstPtr routing_graph_ptr_;
@@ -51,6 +83,8 @@ private:
 
   lanelet::ConstLanelets all_route_lanelets_;  //<! all route lanelets, the order is not defined
   lanelet::ConstLanelets preferred_lanelets_;
+  lanelet::ConstLanelet start_lanelet_;
+  lanelet::ConstLanelet goal_lanelet_;
 
   geometry_msgs::msg::Pose current_pose_;
   lanelet::ConstLanelet current_lanelet_;
--- common/autoware_lanelet2_utils/src/route_manager.cpp
+++ common/autoware_lanelet2_utils/src/route_manager.cpp
@@ -12,10 +12,77 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <autoware/lanelet2_utils/conversion.hpp>
+#include <autoware/lanelet2_utils/nn_search.hpp>
 #include <autoware/lanelet2_utils/route_manager.hpp>
 
+#include <lanelet2_core/LaneletMap.h>
+
 #include <optional>
+#include <tuple>
+#include <utility>
 
 namespace autoware::experimental::lanelet2_utils
 {
+
+std::optional<RouteManager> RouteManager::create(
+  const autoware_map_msgs::msg::LaneletMapBin & map_msg,
+  const autoware_planning_msgs::msg::LaneletRoute & route_msg,
+  const geometry_msgs::msg::Pose & initial_pose)
+{
+  const auto lanelet_map = from_autoware_map_msgs(map_msg);
+  if (!lanelet_map) {
+    return std::nullopt;
+  }
+  const auto [routing_graph, traffic_rules] =
+    instantiate_routing_graph_and_traffic_rules(lanelet_map);
+
+  lanelet::ConstLanelets all_route_lanelets;
+  lanelet::ConstLanelets preferred_lanelets;
+  for (const auto & route_segment : route_msg.segments) {
+    for (const auto & primitive : route_segment.primitives) {
+      const auto id = primitive.id;
+      const auto & llt = lanelet_map->laneletLayer.get(id);
+      if (id == route_segment.preferred_primitive.id) {
+        preferred_lanelets.push_back(llt);
+      }
+      all_route_lanelets.push_back(llt);
+    }
+  }
+
+  const auto & first_route_segment = route_msg.segments.front();
+  const auto start_lanelet =
+    lanelet_map->laneletLayer.get(first_route_segment.preferred_primitive.id);
+  const auto & last_route_segment = route_msg.segments.back();
+  const auto goal_lanelet =
+    lanelet_map->laneletLayer.get(last_route_segment.preferred_primitive.id);
+  const auto closest_lanelet_opt = get_closest_lanelet(all_route_lanelets, initial_pose);
+  if (!closest_lanelet_opt) {
+    return std::nullopt;
+  }
+
+  return RouteManager(
+    lanelet_map, routing_graph, traffic_rules, std::move(all_route_lanelets),
+    std::move(preferred_lanelets), start_lanelet, goal_lanelet, initial_pose,
+    closest_lanelet_opt.value());
+}
+
+RouteManager::RouteManager(
+  lanelet::LaneletMapConstPtr lanelet_map_ptr,
+  lanelet::routing::RoutingGraphConstPtr routing_graph_ptr,
+  lanelet::traffic_rules::TrafficRulesPtr traffic_rules_ptr,
+  lanelet::ConstLanelets && all_route_lanelets, lanelet::ConstLanelets && preferred_lanelets,
+  const lanelet::ConstLanelet & start_lanelet, const lanelet::ConstLanelet & goal_lanelet,
+  const geometry_msgs::msg::Pose & current_pose, lanelet::ConstLanelet current_lanelet)
+: lanelet_map_ptr_(lanelet_map_ptr),
+  routing_graph_ptr_(routing_graph_ptr),
+  traffic_rules_ptr_(traffic_rules_ptr),
+  all_route_lanelets_(std::move(all_route_lanelets)),
+  preferred_lanelets_(std::move(preferred_lanelets)),
+  start_lanelet_(start_lanelet),
+  goal_lanelet_(goal_lanelet),
+  current_pose_(current_pose),
+  current_lanelet_(current_lanelet)
+{
+}
 }  // namespace autoware::experimental::lanelet2_utils
