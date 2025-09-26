c1c74a7094bb5b9d40b0401414e6d265a2d8ffa6
Jakub Zeifert <jakub.zeifert@gmail.com>
2025-09-26 15:12:03 +0200

    linting
    Signed-off-by: Jakub Zeifert <jakub.zeifert@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
@@ -68,7 +68,7 @@ std::optional<geometry_msgs::msg::Point> checkCollision(
     // collision is not one point.
     return std::nullopt;
   }
-  
+
   const double t1 = ((p4.y - p3.y) * (p4.x - p1.x) - (p4.x - p3.x) * (p4.y - p1.y)) / det;
   const double t2 = ((p2.x - p1.x) * (p4.y - p1.y) - (p2.y - p1.y) * (p4.x - p1.x)) / det;
 
