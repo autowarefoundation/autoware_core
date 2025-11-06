0e33982f537aa0cc51a5181a8490efdaea6ad8c8
Jakub Zeifert <74156917+Qubi0-0@users.noreply.github.com>
2025-11-06 08:16:49 +0100

    Update planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
    Co-authored-by: Junya Sasaki <j2sasaki1990@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
@@ -82,6 +82,8 @@ std::optional<geometry_msgs::msg::Point> checkCollision(
   const double t1 = ((p4.y - p3.y) * (p4.x - p1.x) - (p4.x - p3.x) * (p4.y - p1.y)) / det;
   const double t2 = ((p2.x - p1.x) * (p4.y - p1.y) - (p2.y - p1.y) * (p4.x - p1.x)) / det;
 
+  // Helper function to check whether the intersection parameter is slightly
+  // outside the [0, 1] range, considering a small tolerance margin.
   const auto is_outside_interval = [&](const double t) {
     return t < -geometric_epsilon || (1.0 + geometric_epsilon) < t;
   };
