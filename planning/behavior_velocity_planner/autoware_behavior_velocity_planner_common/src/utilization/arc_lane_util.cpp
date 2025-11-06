15f735b32c8e59f907c640ad3833c13d6e4c8da7
Jakub Zeifert <74156917+Qubi0-0@users.noreply.github.com>
2025-11-06 08:16:24 +0100

    Update planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
    Co-authored-by: Junya Sasaki <j2sasaki1990@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/src/utilization/arc_lane_util.cpp
@@ -78,6 +78,7 @@ std::optional<geometry_msgs::msg::Point> checkCollision(
     return std::nullopt;
   }
 
+  // Compute the intersection parameters (t1, t2) along each segment.
   const double t1 = ((p4.y - p3.y) * (p4.x - p1.x) - (p4.x - p3.x) * (p4.y - p1.y)) / det;
   const double t2 = ((p2.x - p1.x) * (p4.y - p1.y) - (p2.y - p1.y) * (p4.x - p1.x)) / det;
 
