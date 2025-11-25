918f482eca50c4c2bef322b8c99c8978becf5fb4
Motsu-san <83898149+Motsu-san@users.noreply.github.com>
2025-11-25 21:28:03 +0900

    refactor: move diagnostics.hpp to source folder
    Signed-off-by: Motsu-san <83898149+Motsu-san@users.noreply.github.com>

--- localization/autoware_ekf_localizer/test/test_diagnostics.cpp
+++ localization/autoware_ekf_localizer/test/test_diagnostics.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "autoware/ekf_localizer/diagnostics.hpp"
+#include "../src/diagnostics.hpp"
 
 #include <gtest/gtest.h>
 
