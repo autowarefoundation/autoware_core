6f0bd51efc9b22bffd41505c6d75ba8d90799f11
Mamoru Sobue <mamoru.sobue@tier4.jp>
2025-11-28 16:42:19 +0900

    it seems from_P1 test fails because of too short lanelet
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- common/autoware_trajectory/test/test_reference_path_vm_10_spec.cpp
+++ common/autoware_trajectory/test/test_reference_path_vm_10_spec.cpp
@@ -1235,7 +1235,7 @@ TEST_P(TestWithVM_01_10_12_Map, from_P6_on_entire_lanes)
 #endif
 }
 
-TEST_P(TestWithVM_01_10_12_Map, DISABLED_from_P1_forward_on_entire_lanes)
+TEST_P(TestWithVM_01_10_12_Map, from_P1_forward_on_entire_lanes)
 {
   const std::vector<lanelet::Id> ids = {60, 57, 56, 58, 59, 55};
   const auto ego_pose = P1;
@@ -1282,7 +1282,13 @@ TEST_P(TestWithVM_01_10_12_Map, DISABLED_from_P1_forward_on_entire_lanes)
     ranges::view::filter([&](const auto & point) { return point.lane_ids.size() == 2; }) |
     ranges::to<std::vector>();
   // TODO(soblin): since Lanelet 57 is too short, this case causes some numerical error, and we
-  // obtain backward path to some extent. So this is DISABLED
+  // obtain backward path to some extent.
+  if (
+    GetParam() == "test_reference_path_valid_03.yaml" ||
+    GetParam() == "test_reference_path_valid_05.yaml") {
+    GTEST_SKIP() << "skip";
+  }
+
   ASSERT_EQ(border_points.size(), 4);
   {
     // 2nd border point
@@ -1388,9 +1394,150 @@ TEST_P(TestWithVM_01_10_12_Map, DISABLED_from_P1_forward_on_entire_lanes)
 #endif
 }
 
+TEST_P(TestWithVM_01_10_12_Map, from_P2_forward_on_entire_lanes)
+{
+  const std::vector<lanelet::Id> ids = {60, 57, 56, 58, 59, 55};
+  const auto ego_pose = P2;
+  const auto current_lanelet = lanelet_map_->laneletLayer.get(56);
+  const auto lanelet_sequence =
+    ids |
+    ranges::views::transform([&](const auto & id) { return lanelet_map_->laneletLayer.get(id); }) |
+    ranges::to<std::vector>();
+
+  const double forward_length = inf;
+  const double backward_length = 0.0;
+  const auto reference_path_opt = trajectory::build_reference_path(
+    lanelet_sequence, current_lanelet, ego_pose, lanelet_map_, routing_graph_, traffic_rules_,
+    forward_length, backward_length);
+  ASSERT_TRUE(reference_path_opt.has_value());
+
+  const auto & reference_path = reference_path_opt.value();
+
+  const auto path_points_with_lane_id = reference_path.restore();
+
+  //
+  //  points are smooth
+  //
+  for (const auto [p1, p2, p3] : ranges::views::zip(
+         path_points_with_lane_id, path_points_with_lane_id | ranges::views::drop(1),
+         path_points_with_lane_id | ranges::views::drop(2))) {
+    EXPECT_TRUE(
+      autoware_utils_geometry::calc_distance3d(p1, p2) >=
+      autoware::experimental::trajectory::k_points_minimum_dist_threshold);
+    EXPECT_TRUE(
+      std::fabs(
+        autoware_utils_math::normalize_radian(
+          autoware_utils_geometry::calc_azimuth_angle(
+            p1.point.pose.position, p2.point.pose.position) -
+          autoware_utils_geometry::calc_azimuth_angle(
+            p2.point.pose.position, p3.point.pose.position))) < M_PI / 2.0);
+  }
+
+  //
+  //  All of path points are either non-border point(with 1 lane_id) or border point(with 2 lane_id)
+  //
+  const auto border_points =
+    path_points_with_lane_id |
+    ranges::view::filter([&](const auto & point) { return point.lane_ids.size() == 2; }) |
+    ranges::to<std::vector>();
+  ASSERT_EQ(border_points.size(), 3);
+  {
+    // 3rd border pointa
+    // 56 -> 58
+    const auto & border_point = border_points.at(0);
+    ASSERT_EQ(border_point.lane_ids.front(), 56);
+    ASSERT_EQ(border_point.lane_ids.back(), 58);
+  }
+  {
+    // 4th border point
+    // 58 -> 59
+    const auto & border_point = border_points.at(1);
+    ASSERT_EQ(border_point.lane_ids.front(), 58);
+    ASSERT_EQ(border_point.lane_ids.back(), 59);
+  }
+  {
+    // 5th border point
+    // 59 -> 55
+    const auto & border_point = border_points.at(2);
+    ASSERT_EQ(border_point.lane_ids.front(), 59);
+    ASSERT_EQ(border_point.lane_ids.back(), 55);
+  }
+
+  const auto non_border_points =
+    path_points_with_lane_id |
+    ranges::view::filter([&](const auto & point) { return point.lane_ids.size() == 1; }) |
+    ranges::to<std::vector>();
+  ASSERT_EQ(non_border_points.size(), path_points_with_lane_id.size() - border_points.size());
+
+  //
+  //  Velocity of path points is set the Lanelet speed limit, and increase at the border point in
+  //  step-function manner
+  //
+  {
+    const auto & non_border_point = non_border_points.at(0);
+    ASSERT_FLOAT_EQ(non_border_point.point.longitudinal_velocity_mps, 20 / 3.6);
+  }
+  {
+    // 3rd border pointa
+    // 56 -> 58
+    const auto & border_point = border_points.at(0);
+    ASSERT_FLOAT_EQ(border_point.point.longitudinal_velocity_mps, 25 / 3.6);
+  }
+  {
+    // 4th border point
+    // 58 -> 59
+    const auto & border_point = border_points.at(1);
+    ASSERT_FLOAT_EQ(border_point.point.longitudinal_velocity_mps, 30 / 3.6);
+  }
+  {
+    // 5th border point
+    // 59 -> 55
+    const auto & border_point = border_points.at(2);
+    ASSERT_FLOAT_EQ(border_point.point.longitudinal_velocity_mps, 35 / 3.6);
+  }
+
+#ifdef PLOT
+  {
+    auto plt = autoware::pyplot::import();
+    auto [fig, axes] = plt.subplots(1, 1);
+    auto & ax = axes[0];
+
+    auto path_plot_config = autoware::test_utils::PathWithLaneIdConfig::defaults();
+    path_plot_config.linewidth = 2.0;
+    path_plot_config.color = "orange";
+    path_plot_config.quiver_size = 2.0;
+
+    auto lane_plot_config = autoware::test_utils::LaneConfig::defaults();
+    lane_plot_config.line_config = autoware::test_utils::LineConfig::defaults();
+
+    autoware_internal_planning_msgs::msg::PathWithLaneId path;
+    path.points = reference_path.restore();
+    autoware::test_utils::plot_autoware_object(path, ax, path_plot_config);
+    ax.set_title(Args(
+      fmt::format(
+        "forward = {}, backward = {}(actual length = {})", forward_length, backward_length,
+        reference_path.length())));
+    for (const auto & route_lanelet : lanelet_sequence) {
+      autoware::test_utils::plot_lanelet2_object(route_lanelet, ax, lane_plot_config);
+    }
+    ax.scatter(Args(ego_pose.position.x, ego_pose.position.y), Kwargs("label"_a = "ego position"));
+    ax.set_aspect(Args("equal"));
+    ax.legend();
+    ax.grid();
+    std::string test_name =
+      std::string(::testing::UnitTest::GetInstance()->current_test_info()->name()) + GetParam();
+    std::replace(test_name.begin(), test_name.end(), '/', '_');
+    plt.savefig(Args(test_name + ".svg"));
+  }
+#endif
+}
+
 INSTANTIATE_TEST_SUITE_P(
   ReferencePathWith_VM_01_10_Maps, TestWithVM_01_10_12_Map,
-  ::testing::Values("test_reference_path_valid_03.yaml"));
+  ::testing::Values(
+    "test_reference_path_valid_01.yaml", "test_reference_path_valid_02.yaml",
+    "test_reference_path_valid_03.yaml", "test_reference_path_valid_04.yaml",
+    "test_reference_path_valid_05.yaml"));
 
 }  // namespace autoware::experimental
 
