29ab30ebfb16b7aa1901882da651fe49d0678a35
mitukou1109 <mitukou1109@gmail.com>
2026-01-16 15:24:12 +0900

    add test
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>

--- common/autoware_trajectory/test/test_trajectory_container.cpp
+++ common/autoware_trajectory/test/test_trajectory_container.cpp
@@ -688,3 +688,19 @@ TEST_F(TrajectoryTest, move_assignment)
 
   check_if_equals(*trajectory, trajectory2);
 }
+
+TEST_F(TrajectoryTest, clamp_longitudinal_velocity_max)
+{
+  trajectory->longitudinal_velocity_mps() = 10.0;
+  trajectory->longitudinal_velocity_mps()
+    .range(trajectory->length() / 3, 2.0 * trajectory->length() / 3)
+    .set(5.0);
+  trajectory->clamp_longitudinal_velocity_max(0.0, 8.0);
+  auto point1 = trajectory->compute(0.0);
+  auto point2 = trajectory->compute(trajectory->length() / 2.0);
+  auto point3 = trajectory->compute(trajectory->length());
+
+  EXPECT_FLOAT_EQ(8.0, point1.point.longitudinal_velocity_mps);
+  EXPECT_FLOAT_EQ(5.0, point2.point.longitudinal_velocity_mps);
+  EXPECT_FLOAT_EQ(8.0, point3.point.longitudinal_velocity_mps);
+}
