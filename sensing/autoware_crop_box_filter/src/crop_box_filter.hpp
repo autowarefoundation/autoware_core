a81cc82d554063882398810589656a5a4989c992
Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
2026-03-02 17:17:00 +0900

    chore(crop_box_filter): update copyright information to reflect current ownership
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>

--- sensing/autoware_crop_box_filter/src/crop_box_filter.hpp
+++ sensing/autoware_crop_box_filter/src/crop_box_filter.hpp
@@ -1,5 +1,3 @@
-// Copyright(c) 2025 AutoCore Technology (Nanjing) Co., Ltd. All rights reserved.
-//
 // Copyright 2026 TIER IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
