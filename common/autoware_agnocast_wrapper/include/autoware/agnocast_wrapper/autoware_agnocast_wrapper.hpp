7e549c20cc3605f94d2c70fed56c8352e74b4788
Koichi Imai <koichi.imai.2@tier4.jp>
2026-03-13 10:24:21 +0900

    fix for copilot review
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/autoware_agnocast_wrapper.hpp
+++ common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/autoware_agnocast_wrapper.hpp
@@ -242,7 +242,12 @@ public:
       ros2_sub_ = node->create_subscription<MessageT>(
         topic_name, qos,
         [callback = std::forward<Func>(callback)](std::unique_ptr<MessageT> msg) {
-          callback(message_ptr<MessageT, ownership>(std::move(msg)));
+          if constexpr (ownership == OwnershipType::Unique) {
+            callback(message_ptr<MessageT, ownership>(std::move(msg)));
+          } else {
+            callback(
+              message_ptr<MessageT, ownership>(std::shared_ptr<MessageT>(std::move(msg))));
+          }
         },
         ros2_options);
     }
