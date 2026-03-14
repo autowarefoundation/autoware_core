e9bb502ebdced7deef390298c77e85cc52289c08
Koichi <koichi.imai.2@tier4.jp>
2026-03-15 03:20:48 +0900

    fix cpplint
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/message_filters.hpp
+++ common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/message_filters.hpp
@@ -16,6 +16,11 @@
 
 #include "autoware/agnocast_wrapper/autoware_agnocast_wrapper.hpp"
 
+#include <functional>
+#include <memory>
+#include <string>
+#include <utility>
+
 #ifdef USE_AGNOCAST_ENABLED
 
 #include <agnocast/message_filters/subscriber.hpp>
@@ -25,11 +30,6 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
-
-#include <functional>
-#include <memory>
-#include <string>
-#include <utility>
 
 namespace autoware::agnocast_wrapper
 {
