cdf57490dd11a0643f1303c87af3a9296575e807
Koichi Imai <koichi.imai.2@tier4.jp>
2026-03-16 22:08:30 +0900

    fix to not allow unique ptr in subscription
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/autoware_agnocast_wrapper.hpp
+++ common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/autoware_agnocast_wrapper.hpp
@@ -230,27 +230,22 @@ public:
     const agnocast::SubscriptionOptions & options)
   {
     static_assert(
-      std::is_invocable_v<std::decay_t<Func>, AUTOWARE_MESSAGE_UNIQUE_PTR(MessageT) &&> ||
-        std::is_invocable_v<std::decay_t<Func>, AUTOWARE_MESSAGE_SHARED_PTR(MessageT) &&> ||
+      !std::is_invocable_v<std::decay_t<Func>, AUTOWARE_MESSAGE_UNIQUE_PTR(MessageT) &&>,
+      "AUTOWARE_MESSAGE_UNIQUE_PTR is not supported for Agnocast subscriptions. "
+      "Agnocast uses shared memory, so mutable exclusive ownership is not possible. "
+      "Use AUTOWARE_MESSAGE_SHARED_PTR or AUTOWARE_MESSAGE_CONST_SHARED_PTR instead.");
+
+    static_assert(
+      std::is_invocable_v<std::decay_t<Func>, AUTOWARE_MESSAGE_SHARED_PTR(MessageT) &&> ||
         std::is_invocable_v<std::decay_t<Func>, AUTOWARE_MESSAGE_CONST_SHARED_PTR(MessageT) &&>,
       "callback should be invocable with an rvalue reference to either "
-      "AUTOWARE_MESSAGE_UNIQUE_PTR, "
-      "AUTOWARE_MESSAGE_SHARED_PTR, or AUTOWARE_MESSAGE_CONST_SHARED_PTR");
-
-    constexpr auto ownership =
-      std::is_invocable_v<std::decay_t<Func>, AUTOWARE_MESSAGE_UNIQUE_PTR(MessageT) &&>
-        ? OwnershipType::Unique
-        : OwnershipType::Shared;
+      "AUTOWARE_MESSAGE_SHARED_PTR or AUTOWARE_MESSAGE_CONST_SHARED_PTR");
 
     subscription_ = agnocast::create_subscription<MessageT>(
       node, topic_name, qos,
       [callback = std::forward<Func>(callback)](agnocast::ipc_shared_ptr<MessageT> && msg) {
-        if constexpr (ownership == OwnershipType::Unique) {
-          callback(message_ptr<MessageT, ownership>(std::move(msg)));
-        } else {
-          callback(message_ptr<const MessageT, ownership>(
-            agnocast::ipc_shared_ptr<const MessageT>(std::move(msg))));
-        }
+        callback(message_ptr<const MessageT, OwnershipType::Shared>(
+          agnocast::ipc_shared_ptr<const MessageT>(std::move(msg))));
       },
       options);
   }
