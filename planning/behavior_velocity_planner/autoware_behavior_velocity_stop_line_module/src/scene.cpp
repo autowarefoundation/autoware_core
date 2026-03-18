77a6a0888389703211d7ed467627384e83af22e3
NorahXiong <norah.xiong@autocore.ai>
2026-03-18 15:46:35 +0800

    feat(autoware_behavior_velocity_stop_line_module): improve code robustness by optional value check and setting
    Co-authored-by: Ryohsuke Mitsudome <43976834+mitsudome-r@users.noreply.github.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_stop_line_module/src/scene.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_stop_line_module/src/scene.cpp
@@ -189,8 +189,9 @@ void StopLineModule::updateStateAndStoppedTime(
       break;
     }
     case State::STOPPED: {
-      if (!stopped_time->has_value()) {
-        logWarn("stopped_time has no value in STOPPED state");
+      if (!stopped_time_.has_value()) {
+        logWarn("stopped_time_ has no value in STOPPED state");
+        stopped_time_ = now;
         break;
       }
       double stop_duration = (now - **stopped_time).seconds();
