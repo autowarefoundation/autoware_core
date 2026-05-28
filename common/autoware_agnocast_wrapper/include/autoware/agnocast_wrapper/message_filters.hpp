d9b9cb765d98786c8cc9c70dc7f7812cc9e6987b
Koichi Imai <koichi.imai.2@tier4.jp>
2026-05-29 08:41:18 +0900

    move when capture
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/message_filters.hpp
+++ common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/message_filters.hpp
@@ -258,12 +258,14 @@ private:
 
     // Wrap upstream's Connection so the user's disconnect also drops our adapter entry.
     return ::message_filters::Connection(
-      ::message_filters::Connection::VoidDisconnectFunction([this, adapter,
-                                                             upstream_conn]() mutable {
-        upstream_conn.disconnect();
-        std::lock_guard<std::mutex> lock(adapters_mutex_);
-        adapters_.erase(std::remove(adapters_.begin(), adapters_.end(), adapter), adapters_.end());
-      }));
+      ::message_filters::Connection::VoidDisconnectFunction(
+        [this, adapter = std::move(adapter),
+         upstream_conn = std::move(upstream_conn)]() mutable {
+          upstream_conn.disconnect();
+          std::lock_guard<std::mutex> lock(adapters_mutex_);
+          adapters_.erase(
+            std::remove(adapters_.begin(), adapters_.end(), adapter), adapters_.end());
+        }));
   }
 
   std::mutex adapters_mutex_;
