f8fa7ba9194084182476e8fcbc86a290ed639ba4
Mert Yavuz <mert@adastec.com>
2026-05-31 13:51:03 +0300

    fix(autoware_motion_velocity_planner): handle empty point cloud before is_pcl_time_valid operations to rid of extra computation

--- planning/motion_velocity_planner/autoware_motion_velocity_planner/src/node.cpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_planner/src/node.cpp
@@ -230,16 +230,22 @@ std::optional<pcl::PointCloud<pcl::PointXYZ>>
 MotionVelocityPlannerNode::process_no_ground_pointcloud(
   const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg)
 {
-  geometry_msgs::msg::TransformStamped transform;
-  const bool is_pcl_time_valid = (this->get_clock()->now() - rclcpp::Time(msg->header.stamp)) <
-                                 rclcpp::Duration::from_seconds(1.0);
 
   if (msg->width == 0 || msg->height == 0) {
     RCLCPP_DEBUG_SKIPFIRST_THROTTLE(
       get_logger(), *get_clock(), 5000,  // 5 seconds
       "Received empty no_ground_pointcloud, skipping processing");
-    return std::nullopt;
+    pcl::PointCloud<pcl::PointXYZ> pc_transformed;
+    pc_transformed.header = pcl_conversions::toPCL(msg->header);
+    pc_transformed.header.frame_id = "map";
+    return pc_transformed;
   }
+
+  geometry_msgs::msg::TransformStamped transform;
+  const bool is_pcl_time_valid = (this->get_clock()->now() - rclcpp::Time(msg->header.stamp)) <
+                                 rclcpp::Duration::from_seconds(1.0);
+
+
 
   if (
     is_pcl_time_valid && tf_buffer_.canTransform("map", msg->header.frame_id, msg->header.stamp)) {
