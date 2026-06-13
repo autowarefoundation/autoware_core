fbe90d2d6b33304cba0b186e14ea46f99f639328
TranHuuNhatHuy <huy9515@gmail.com>
2026-06-14 02:40:41 +0900

    isolate create_control_command's core logics from the mess of ROS2+core logics in original node module

--- control/autoware_simple_pure_pursuit/src/simple_pure_pursuit_core_logics.cpp
+++ control/autoware_simple_pure_pursuit/src/simple_pure_pursuit_core_logics.cpp
@@ -21,14 +21,64 @@
 namespace autoware::control::simple_pure_pursuit
 {
 
+    using autoware::motion_utils::findNearestIndex;
+
+    // Constructor
     SimplePurePursuitCoreLogics::SimplePurePursuitCoreLogics(const SimplePurePursuitParameters & params)
     : params_(params)
     {
-    }
+    };
 
+    // Params setter
     void SimplePurePursuitCoreLogics::set_params(const SimplePurePursuitParameters & params)
     {
         params_ = params;
+    };
+
+    // Core logic for creating control command based on current odom and traj
+    autoware_control_msgs::msg::Control SimplePurePursuitCoreLogics::create_control_command(
+        const nav_msgs::msg::Odometry & odom, 
+        const autoware_planning_msgs::msg::Trajectory & traj) const
+    {
+
+        const size_t closest_traj_point_idx = findNearestIndex(traj.points, odom.pose.pose.position);
+
+        // When ego reaches goal or traj is too short, stop vehicle
+        if (
+            (closest_traj_point_idx == traj.points.size() - 1) || 
+            (traj.points.size() <= 5)
+        ) {
+            autoware_control_msgs::msg::Control control_command;
+            control_command.stamp = odom.header.stamp;
+            control_command.longitudinal.velocity = 0.0;
+            control_command.longitudinal.acceleration = -10.0;
+            control_command.longitudinal.is_defined_acceleration = true;
+
+            return control_command;
+        }
+
+        // Calculate target longitudinal velocity
+        const double target_longitudinal_vel =
+            params_.use_external_target_vel 
+            ? params_.external_target_vel
+            : traj.points.at(closest_traj_point_idx).longitudinal_velocity_mps;
+
+        // Calculate control command
+        autoware_control_msgs::msg::Control control_command;
+        control_command.stamp = odom.header.stamp;
+        control_command.longitudinal = calc_longitudinal_control(
+            odom, 
+            target_longitudinal_vel
+        );
+        control_command.lateral = calc_lateral_control(
+            odom, 
+            traj, 
+            target_longitudinal_vel, 
+            closest_traj_point_idx
+        );
+
+        return control_command;
+
     }
 
 }; // namespace autoware::control::simple_pure_pursuit
