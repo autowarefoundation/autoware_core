b52657a9b815b62b591ee0feee13ec6873dd9a24
TranHuuNhatHuy <huy9515@gmail.com>
2026-06-14 00:23:54 +0900

    defined simpupu params struct

--- control/autoware_simple_pure_pursuit/src/simple_pure_pursuit_core_logics.hpp
+++ control/autoware_simple_pure_pursuit/src/simple_pure_pursuit_core_logics.hpp
@@ -11,3 +11,24 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#ifndef SIMPLE_PURE_PURSUIT_CORE_LOGICS_HPP_
+#define SIMPLE_PURE_PURSUIT_CORE_LOGICS_HPP_
+
+#include <autoware_control_msgs/msg/control.hpp>
+#include <autoware_planning_msgs/msg/trajectory.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+namespace autoware::control::simple_pure_pursuit
+{
+
+struct PurePursuitParameters {
+  double lookahead_gain;
+  double lookahead_min_distance;
+  double speed_proportional_gain;
+  bool use_external_target_vel;
+  double external_target_vel;
+  double wheel_base_m;
+};
+
+}
