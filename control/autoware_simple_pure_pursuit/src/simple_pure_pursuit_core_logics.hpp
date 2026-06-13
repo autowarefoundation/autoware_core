400cfb7f8517d0692479e264febe41091809bf67
TranHuuNhatHuy <huy9515@gmail.com>
2026-06-14 00:33:39 +0900

    define public funcs

--- control/autoware_simple_pure_pursuit/src/simple_pure_pursuit_core_logics.hpp
+++ control/autoware_simple_pure_pursuit/src/simple_pure_pursuit_core_logics.hpp
@@ -22,7 +22,17 @@
 namespace autoware::control::simple_pure_pursuit
 {
 
-struct PurePursuitParameters {
+/**
+@ brief Params for SimplePurePursuit class
+*
+* @param lookahead_gain Gain for calculating lookahead distance
+* @param lookahead_min_distance Minimum lookahead distance
+* @param speed_proportional_gain Proportional gain for longitudinal control
+* @param use_external_target_vel Whether to use external target velocity or not
+* @param external_target_vel External target velocity to use when use_external_target_vel is true
+* @param wheel_base_m Wheel base of vehicle in meters
+*/
+struct SimplePurePursuitParameters {
   double lookahead_gain;
   double lookahead_min_distance;
   double speed_proportional_gain;
@@ -31,4 +41,30 @@ struct PurePursuitParameters {
   double wheel_base_m;
 };
 
-}
+class SimplePurePursuitCoreLogics
+{
+
+    public:
+  
+    // Constructor
+    explicit SimplePurePursuitCoreLogics(const SimplePurePursuitParameters & parameters);
+    
+    // Set param struct for algorithm
+    void set_parameters(const SimplePurePursuitParameters & parameters);
+
+    /**
+    * @brief Create control command based on current odom and traj
+    *
+    * @param odom Current odometry of vehicle
+    * @param traj Current trajectory to follow
+    *
+    * @return Control command to be published
+    */
+    autoware_control_msgs::msg::Control create_control_command(
+        const nav_msgs::msg::Odometry & odom, 
+        const autoware_planning_msgs::msg::Trajectory & traj
+    ) const;
+
+};
+
+}; // namespace autoware::control::simple_pure_pursuit
