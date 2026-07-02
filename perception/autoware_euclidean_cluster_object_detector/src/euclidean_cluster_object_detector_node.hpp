971b586c10360f009cd61f69631dd18e141d6474
TranHuuNhatHuy <huy9515@gmail.com>
2026-07-02 23:48:57 +0900

    implemented node wrapper header

--- perception/autoware_euclidean_cluster_object_detector/src/euclidean_cluster_object_detector_node.hpp
+++ perception/autoware_euclidean_cluster_object_detector/src/euclidean_cluster_object_detector_node.hpp
@@ -15,4 +15,41 @@
 #ifndef EUCLIDEAN_CLUSTER_OBJECT_DETECTOR_NODE_HPP_
 #define EUCLIDEAN_CLUSTER_OBJECT_DETECTOR_NODE_HPP_
 
+#include "euclidean_cluster_object_detector.hpp"
+
+#include <autoware_utils_debug/debug_publisher.hpp>
+#include <autoware_utils_system/stop_watch.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <autoware_perception_msgs/msg/detected_objects.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+#include <memory>
+
+namespace autoware::euclidean_cluster
+{
+
+class EuclideanClusterObjectDetectorNode : public rclcpp::Node
+{
+public:
+  explicit EuclideanClusterObjectDetectorNode(const rclcpp::NodeOptions & options);
+
+private:
+  /**
+   * @brief Callback func for point cloud subscription
+   * @param input_msg Point cloud message
+   */
+  void on_point_cloud(const sensor_msgs::msg::PointCloud2::ConstSharedPtr input_msg);
+
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_pointcloud_;
+  rclcpp::Publisher<autoware_perception_msgs::msg::DetectedObjects>::SharedPtr pub_clusters_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_debug_;
+
+  std::unique_ptr<EuclideanClusterObjectDetector> detector_;
+  std::unique_ptr<autoware_utils_system::StopWatch<std::chrono::milliseconds>> stop_watch_;
+  std::unique_ptr<autoware_utils_debug::DebugPublisher> debug_publisher_;
+};
+
+}  // namespace autoware::euclidean_cluster
+
 #endif  // EUCLIDEAN_CLUSTER_OBJECT_DETECTOR_NODE_HPP_
