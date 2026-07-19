5daa07a09c7100ca9449d6207fd826b5fa960ef7
Daisuke Nishimatsu <nishimarudai@gmail.com>
2026-07-19 20:53:50 +1000

    fix(autoware_ground_filter): avoid non-standard math constants
    This upstreams a cleaned-up version of RoboStack patch/ros-rolling-autoware-ground-filter.patch.
    
    autoware_ground_filter used non-standard float math constants such as M_PIf, which are not provided consistently across toolchains. Use local constexpr constants instead, and pass rclcpp::QoS objects directly to message_filters subscriptions to match the current ROS 2 API.
    
    Signed-off-by: Tobias Fischer <info@tobiasfischer.info>

--- perception/autoware_ground_filter/src/ground_filter_node.cpp
+++ perception/autoware_ground_filter/src/ground_filter_node.cpp
@@ -189,9 +189,9 @@ void GroundFilterComponent::subscribe()
   if (use_indices_) {
     // Subscribe to the input using a filter
     sub_input_filter_.subscribe(
-      this, "input", rclcpp::SensorDataQoS().keep_last(max_queue_size_).get_rmw_qos_profile());
+      this, "input", rclcpp::SensorDataQoS().keep_last(max_queue_size_));
     sub_indices_filter_.subscribe(
-      this, "indices", rclcpp::SensorDataQoS().keep_last(max_queue_size_).get_rmw_qos_profile());
+      this, "indices", rclcpp::SensorDataQoS().keep_last(max_queue_size_));
 
     if (approximate_sync_) {
       sync_input_indices_a_ = std::make_shared<
