bd6442dfff21ebff3954f6a09ad794726af18259
TranHuuNhatHuy <huy9515@gmail.com>
2026-08-07 01:08:38 +0900

    revamped ekf_localizer.hpp header - removed rclcpp timestamps, new corewarnings
    Signed-off-by: TranHuuNhatHuy <huy9515@gmail.com>

--- localization/autoware_ekf_localizer/src/ekf_localizer.hpp
+++ localization/autoware_ekf_localizer/src/ekf_localizer.hpp
@@ -21,7 +21,6 @@
 
 #include <autoware/kalman_filter/kalman_filter.hpp>
 #include <autoware/kalman_filter/time_delay_kalman_filter.hpp>
-#include <rclcpp/rclcpp.hpp>
 #include <tf2/utils.hpp>
 
 #include <geometry_msgs/msg/pose_stamped.hpp>
@@ -30,7 +29,9 @@
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
 
+#include <array>
 #include <memory>
+#include <string>
 #include <vector>
 
 namespace autoware::ekf_localizer
@@ -48,6 +49,11 @@ struct EKFDiagnosticInfo
   double mahalanobis_distance{0.0};
 };
 
+struct CoreWarning {
+  std::string text;
+  uint32_t throttle_ms{0};
+};
+
 class Simple1DFilter
 {
 public:
@@ -58,12 +64,14 @@ public:
     var_ = 1e9;
     proc_var_x_c_ = 0.0;
   };
+
   void init(const double init_obs, const double obs_var)
   {
     x_ = init_obs;
     var_ = obs_var;
     initialized_ = true;
   };
+
   void update(const double obs, const double obs_var, const double dt)
   {
     if (!initialized_) {
@@ -80,6 +88,7 @@ public:
     x_ = x_ + kalman_gain * (obs - x_);
     var_ = (1 - kalman_gain) * var_;
   };
+
   void set_proc_var(const double proc_var) { proc_var_x_c_ = proc_var; }
   [[nodiscard]] double get_x() const { return x_; }
   [[nodiscard]] double get_var() const { return var_; }
@@ -100,40 +109,51 @@ private:
   using Twist = geometry_msgs::msg::TwistStamped;
 
 public:
-  EKFModule(std::shared_ptr<Warning> warning, const HyperParameters & params);
+  explicit EKFModule(const HyperParameters & params);
 
   void initialize(
     const PoseWithCovariance & initial_pose,
     const geometry_msgs::msg::TransformStamped & transform);
 
-  [[nodiscard]] geometry_msgs::msg::PoseStamped get_current_pose(
-    const rclcpp::Time & current_time, bool get_biased_yaw) const;
-  [[nodiscard]] geometry_msgs::msg::TwistStamped get_current_twist(
-    const rclcpp::Time & current_time) const;
+  [[nodiscard]] geometry_msgs::msg::PoseStamped get_current_pose(bool get_biased_yaw) const;
+  [[nodiscard]] geometry_msgs::msg::TwistStamped get_current_twist() const;
   [[nodiscard]] double get_yaw_bias() const;
   [[nodiscard]] std::array<double, 36> get_current_pose_covariance() const;
   [[nodiscard]] std::array<double, 36> get_current_twist_covariance() const;
 
   [[nodiscard]] size_t find_closest_delay_time_index(double target_value) const;
+  
   void accumulate_delay_time(const double dt);
-
   void predict_with_delay(const double dt);
+
   bool measurement_update_pose(
-    const PoseWithCovariance & pose, const rclcpp::Time & t_curr,
-    EKFDiagnosticInfo & pose_diag_info);
+    const PoseWithCovariance & pose, 
+    const double & t_curr_sec,
+    EKFDiagnosticInfo & pose_diag_info,
+    std::vector<CoreWarning> & warnings_out
+  );
+
   bool measurement_update_twist(
-    const TwistWithCovariance & twist, const rclcpp::Time & t_curr,
-    EKFDiagnosticInfo & twist_diag_info);
+    const TwistWithCovariance & twist, 
+    const double t_curr_sec,
+    EKFDiagnosticInfo & twist_diag_info, 
+    std::vector<CoreWarning> & warnings_out
+  );
+
   geometry_msgs::msg::PoseWithCovarianceStamped compensate_rph_with_delay(
-    const PoseWithCovariance & pose, tf2::Vector3 last_angular_velocity, const double delay_time);
+    const PoseWithCovariance & pose, 
+    tf2::Vector3 last_angular_velocity, 
+    const double delay_time
+  );
 
 private:
   void update_simple_1d_filters(
-    const geometry_msgs::msg::PoseWithCovarianceStamped & pose, const size_t smoothing_step);
+    const geometry_msgs::msg::PoseWithCovarianceStamped & pose, 
+    const size_t smoothing_step
+  );
 
   TimeDelayKalmanFilter kalman_filter_;
 
-  std::shared_ptr<Warning> warning_;
   const int dim_x_;
   std::vector<double> accumulated_delay_times_;
   const HyperParameters params_;
