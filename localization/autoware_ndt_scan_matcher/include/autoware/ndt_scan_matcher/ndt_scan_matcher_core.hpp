21860aa244fb6905de4ef397986cb27097edb151
Junya Sasaki <j2sasaki1990@gmail.com>
2026-09-01 14:47:43 +0900

    fix: do not use optional for `stamp` in publish logic
    Signed-off-by: Junya Sasaki <junya.sasaki@tier4.jp>
    
    Co-authored-by: Motz <83898149+Motsu-san@users.noreply.github.com>

--- localization/autoware_ndt_scan_matcher/include/autoware/ndt_scan_matcher/ndt_scan_matcher_core.hpp
+++ localization/autoware_ndt_scan_matcher/include/autoware/ndt_scan_matcher/ndt_scan_matcher_core.hpp
@@ -139,7 +139,7 @@ private:
     const geometry_msgs::msg::PoseWithCovarianceStamped & initial_pose_old_msg,
     const geometry_msgs::msg::PoseWithCovarianceStamped & initial_pose_new_msg);
   void publish_loaded_map_if_present(
-    MapUpdateModule::UpdateResult & result, const std::optional<rclcpp::Time> & stamp) const;
+    MapUpdateModule::UpdateResult & result, const rclcpp::Time & stamp) const;
 
   static int count_oscillation(const std::vector<geometry_msgs::msg::Pose> & result_pose_msg_array);
 
