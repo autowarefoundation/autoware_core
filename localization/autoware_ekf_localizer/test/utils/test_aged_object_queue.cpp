b2882e5882387b4a30f785d52b59f91203a58150
TranHuuNhatHuy <huy9515@gmail.com>
2026-09-24 11:34:40 +0900

    added another test case for pose and twist queuing mechanics
    Signed-off-by: TranHuuNhatHuy <huy9515@gmail.com>

--- localization/autoware_ekf_localizer/test/utils/test_aged_object_queue.cpp
+++ localization/autoware_ekf_localizer/test/utils/test_aged_object_queue.cpp
@@ -126,4 +126,22 @@ TEST(AgedObjectQueue, PopAndMaxQueueSize)
   EXPECT_TRUE(queue.empty());
 }
 
+TEST(AgedObjectQueue, PoseAndTwistQueues)
+{
+  using PoseMsg = geometry_msgs::msg::PoseWithCovarianceStamped::ConstSharedPtr;
+  using TwistMsg = geometry_msgs::msg::TwistWithCovarianceStamped::ConstSharedPtr;
+
+  AgedObjectQueue<PoseMsg> pose_queue(3, 5);
+  EXPECT_EQ(pose_queue.max_queue_size(), 5U);
+  auto p = std::make_shared<geometry_msgs::msg::PoseWithCovarianceStamped>();
+  pose_queue.push(p);
+  EXPECT_EQ(pose_queue.pop(), p);
+
+  AgedObjectQueue<TwistMsg> twist_queue(3, 5);
+  EXPECT_EQ(twist_queue.max_queue_size(), 5U);
+  auto t = std::make_shared<geometry_msgs::msg::TwistWithCovarianceStamped>();
+  twist_queue.push(t);
+  EXPECT_EQ(twist_queue.pop(), t);
+}
+
 }  // namespace autoware::ekf_localizer
