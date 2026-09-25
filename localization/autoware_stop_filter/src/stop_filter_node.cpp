c64789aa4d839fd927cff9284958ef3b075c3a40
Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
2026-09-25 16:44:40 +0900

    refactor(autoware_stop_filter): move StopFilterResult fields into output messages
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>

--- localization/autoware_stop_filter/src/stop_filter_node.cpp
+++ localization/autoware_stop_filter/src/stop_filter_node.cpp
@@ -36,14 +36,14 @@ StopFilterNode::StopFilterNode(const rclcpp::NodeOptions & node_options)
 void StopFilterNode::callback_odometry(
   const AUTOWARE_MESSAGE_CONST_SHARED_PTR(nav_msgs::msg::Odometry) & msg)
 {
-  const auto result = stop_filter_.filter(*msg);
+  auto result = stop_filter_.filter(*msg);
 
   auto stop_flag_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_stop_flag_);
-  *stop_flag_msg = result.stop_flag;
+  *stop_flag_msg = std::move(result.stop_flag);
   pub_stop_flag_->publish(std::move(stop_flag_msg));
 
   auto filtered_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_odom_);
-  *filtered_msg = result.filtered_odometry;
+  *filtered_msg = std::move(result.filtered_odometry);
   pub_odom_->publish(std::move(filtered_msg));
 }
 }  // namespace autoware::stop_filter
